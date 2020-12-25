7c53a3b24ea068f8d453ab36fbb228c1c9c458d2
Ilya B <ilbv@mail.ru>
2020-12-26 01:45:23 +0300

    Home work for Stepik

--- test_items.py
+++ test_items.py
@@ -1,10 +1,18 @@
-import unittest
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+from selenium.common import exceptions
+from selenium.webdriver.common.by import By
+import time
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class ButtonNotFound(Exception):
+    pass
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_find_add_to_cart_button(browser):
+    browser.get(link)
+    try:
+        # time.sleep(30)
+        button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket').text
+        assert len(button) > 0, 'Кнопка не найдена'
+    except exceptions.NoSuchElementException:
+        raise ButtonNotFound('Элемент отсутствует на странице')
